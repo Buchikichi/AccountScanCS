1233b073ef73ccdd2b059e713ebbccab11229faf
Hidetaka Sasai <sasai@ds-solution.co.jp>
2018-09-11 16:12:04 +0900

    Modify OcrUtils.

--- AccountScan/Util/OcrUtils.cs
+++ AccountScan/Util/OcrUtils.cs
@@ -2,6 +2,8 @@
 using Emgu.CV.CvEnum;
 using Emgu.CV.OCR;
 using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AccountScan.Util
 {
@@ -12,7 +14,7 @@ namespace AccountScan.Util
         public static Tesseract.Character[] Recognize(Mat mat, string lang = "eng")
         {
             var baseDir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\').Replace('\\', '/');
-            var testData = baseDir + TESTDATA;
+            var testData = GetShortPathName(baseDir + TESTDATA);
 
             CvInvoke.Threshold(mat, mat, 120, 255, ThresholdType.Binary);
             using (var tesseract = new Tesseract(testData, lang, OcrEngineMode.TesseractOnly))
@@ -23,5 +25,19 @@ namespace AccountScan.Util
                 return tesseract.GetCharacters();
             }
         }
+
+        #region kernel32
+        private static string GetShortPathName(string longPath)
+        {
+            var bufferSize = 260;
+            var buff = new StringBuilder(bufferSize);
+
+            GetShortPathName(longPath, buff, bufferSize);
+            return buff.ToString();
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetShortPathName(string longPath, StringBuilder shortPathBuffer, int bufferSize);
+        #endregion
     }
 }
